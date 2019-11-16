6d5f3d07cbcef12ae23e016ec4ea18996c2c12f9
TaeminHa <tommy7507@gmail.com>
2019-11-16 17:07:34 -0600

    added 5 final variables that'll be used for CO2 emission
    houseSize - double
    state - String
    time - int
    gas - double
    electricity - double

--- lib/dataEntry_page.dart
+++ lib/dataEntry_page.dart
@@ -18,16 +18,22 @@ class _dataEntryState extends State<dataEntry> {
   double houseSize;
   String state;
   int time;
+  double gas;
+  double electricity;
 
   final sizeHouseControl = new TextEditingController();
   final stateControl = new TextEditingController();
-  final timeControl = new TextEditingController();
+  final ACtimeControl = new TextEditingController();
+  final gasburnerControl = new TextEditingController();
+  final electricityControl = new TextEditingController();
+
 
   void setVariables () {
     houseSize = double.parse(sizeHouseControl.text);
     state = stateControl.text;
-    time = int.parse(timeControl.text);
-
+    time = int.parse(ACtimeControl.text);
+    gas = double.parse(gasburnerControl.text);
+    electricity = double.parse(electricityControl.text);
   }
 
   _dataEntryState(this.houseSize, this.state, this.time);
@@ -40,51 +46,101 @@ class _dataEntryState extends State<dataEntry> {
         backgroundColor: Colors.orangeAccent,
       ),
       body:
-      new ListView(
-        children: <Widget>[
-          SizedBox(
-            height: 200,
-            width: 360,
-            child: ListView(
-              children: <Widget>[
-                Text(
-                    'What is the size of your house?'
-                ),
-                TextField(
-                  controller: sizeHouseControl,
-                )
-              ],
+      new Container(
+        alignment: AlignmentDirectional(0.0, 0.0),
+        height: 1000,
+        child: ListView(
+          children: <Widget>[
+
+            SizedBox(
+              height: 150,
+              width: 360,
             ),
-          ),
-          SizedBox(
-            height: 200,
-            width: 360,
-            child: ListView(
-              children: <Widget>[
-                Text(
-                    'What is the state you live in?'
-                ),
-                TextField(
-                  controller: stateControl,
-                )
-              ],
+            SizedBox(
+              height: 80,
+              width: 360,
+              child: Column(
+
+                children: <Widget>[
+                  Text(
+                      'What is the size of your house?'
+                  ),
+                  TextField(
+                    controller: sizeHouseControl,
+                  )
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 200,
-            width: 360,
-            child: ListView(
-              children: <Widget>[
-                Text(
-                    'What is the time?'
-                ),
-                TextField(
-                  controller: timeControl,
-                )
-              ],
+            SizedBox(
+              height: 80,
+              width: 360,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                      'What is your gas burner usage?'
+                  ),
+                  TextField(
+                    controller: gasburnerControl,
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 80,
+              width: 360,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                      'What is your electricity usage?'
+                  ),
+                  TextField(
+                    controller: electricityControl,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 80,
+              width: 360,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                      'What is the state you live in?'
+                  ),
+                  TextField(
+                    controller: stateControl,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 80,
+              width: 360,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                      'What long do you use the A.C?'
+                  ),
+                  TextField(
+                    controller: ACtimeControl,
+                  )
+                ],
+              ),
+            ),
+            FlatButton(
+              child: Text('Submit'),
+              color: Colors.orangeAccent,
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+              onPressed: () => {
+                /*TODO*/
+                //implement submit
+              },
+            )
+          ],
+        ),
       )
     );
   }
