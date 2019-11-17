a318cd27d4ea6c5b298dd1d8bf3fdbda59842e61
PrasannS <singhal.prasann07@gmail.com>
2019-11-16 21:56:29 -0600

    API code officially working now!

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @mustCallSuper
   void initState(){
     super.initState();
-    fetchAPIResult("hi");
+    //fetchAPIResult("hi");
   }
 
   GoogleMapController mapController;
